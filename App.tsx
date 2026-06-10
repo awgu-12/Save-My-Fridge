7880ae071a6c713c4bb921c150406789014e5876
구민주 <minju5150@naver.com>
2026-06-11 02:47:18 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://save-my-fridge.vercel.app' }} 
+        style={{ flex: 1 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
